58aae9b97cafc2e526803189997ff97a1e671a18
Aravindsuramalla <aravindsuramalla99@gmail.com>
2023-02-12 00:10:09 +0530

    delete

--- myjs.js
+++ myjs.js
@@ -1,38 +1,87 @@
+var ul=document.getElementById('list-of-users');
+ul.addEventListener('click',deleteUsers);
+//ul.addEventListener('click',editUsers);
 document.addEventListener('DOMContentLoaded',loadItems);
-function loadItems()
+function deleteUsers(e)
 {
-    let ul=document.querySelector('#list-of-users');
+    if(e.target.classList.contains('delete-users'))
+    {
+        //console.log(e.target.parentElement.innerHTML.split('-')[0]);
+        ul.removeChild(e.target.parentElement);
+        localStorage.removeItem(e.target.parentElement.innerHTML.split('-')[0]);
+    }
+}
+/*function editUsers(e)
+{
+    if(e.target.classList.contains('edit-users'))
+    {
+        //console.log(e.target.parentElement.innerHTML.split('-')[0]);
+        ul.removeChild(e.target.parentElement);
+        let item=JSON.parse(localStorage.getItem(e.target.parentElement.innerHTML.split('-')[0]));
+        document.forms['user-data']['name'].value=item.name;
+        document.forms['user-data']['email'].value=item.email;
+        document.forms['user-data']['Phone'].value=item.number;
+        document.forms['user-data']['tfc'].value=item.Date;
+        document.forms['user-data']['time'].value=item.Time;
+        localStorage.removeItem(e.target.parentElement.innerHTML.split('-')[0]);
+
+    }
+}*/
+function loadItems()
+{ 
+    
     for(x in localStorage){
         if(x=='length')
             break;
         var value = localStorage.getItem(x);
         console.log(value);
         value = JSON.parse(value);
-        //var li = createNewLi(value.item, value.desc);
+        var li = createNewLi(value);
         //itemList.appendChild(li);
-        var li=document.createElement('li');
-        li.className='user_records';
-        li.appendChild(document.createTextNode(value.name));  
-        li.appendChild(document.createTextNode(value.email));
-        li.appendChild(document.createTextNode(value.number));
-        li.appendChild(document.createTextNode(value.Date));
-        li.appendChild(document.createTextNode(value.Time));
-        console.log(li);
+       
+        //console.log(li);
         ul.append(li);
-        //new_div.append(value.name);
+        //bt.onclick=deleteUsers();
+      //new_div.append(value.name);
     }
 }
+function createNewLi(value)
+{
+    var li=document.createElement('li');
+    li.className='user_records';
+    li.appendChild(document.createTextNode(value.name+'-'));  
+    li.appendChild(document.createTextNode(value.email+'-')); 
+    li.appendChild(document.createTextNode(value.number+'-'));
+    li.appendChild(document.createTextNode(value.Date+'-'));
+    li.appendChild(document.createTextNode(value.Time));
+    var bt=document.createElement('button');
+    bt.className='delete-users';
+    bt.id='delUser';
+    bt.setAttribute('value','Delete');
+    bt.appendChild(document.createTextNode('delete'));
+    li.appendChild(bt);
+    var edit=document.createElement('button');
+    edit.className='edit-users';
+    edit.id='editUser';
+    edit.setAttribute('value','Edit');
+    edit.appendChild(document.createTextNode('Edit'));
+    li.appendChild(edit);
+
+    return li;
+}
+
 function print(e)
 {
     e.preventDefault();
     let name=document.getElementById('name').value;
     let email=document.getElementById('email').value;
     let number=document.getElementById('Phone').value;
-    let date=new Date(document.getElementById("tfc").value);
-    let time=document.getElementById("time").value;
-    let d=date.getDate();
-    let m=date.getMonth()+1;
-    let y=date.getFullYear();
+    let date=document.getElementById("tfc").value;
+    //let time=document.getElementById("time").value;
+    //let d=date.getDate();
+    //let m=date.getMonth()+1;
+    //let y=date.getFullYear();
+    console.log(date);
     let Time=document.getElementById('time').value;
    
     //let user_records=new Array();
@@ -43,11 +92,15 @@ function print(e)
         'name': name,
         'email': email,
         'number': number,
-        'Date': `${d}-${m}-${y}`,
+        'Date': date,
         'Time': Time
     };
 
     localStorage.setItem(user_records.name,JSON.stringify(user_records));
+
+    var li = createNewLi(user_records);
+       
+    ul.append(li);
 
     
     console.log(user_records);
@@ -83,6 +136,7 @@ function print(e)
     
     //localStorage.removeItem(user_records[0]);
     //console.log(localStorage.getItem('myObj'));
+    document.forms['user-data'].reset();
 }
 
 
